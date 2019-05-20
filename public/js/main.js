6d98dee361245fb390d09f725d36f219e254dd24
John <molendykj@gmail.com>
2019-05-19 21:20:58 -0700

    no message

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,8 @@ function getURLParameters(whichParam)
 	var pageURL = window.location.search.substring(1);
 	var pageURLVariables = pageURL.split('&');
 	for(var i = 0; i < pageURLVariables.length; i++){
-		var parameterName = pageURLVariables(i).split('=';
-		if(parameterName(0) == whichParam{
+		var parameterName = pageURLVariables(i).split('=');
+		if(parameterName(0) == whichParam){
 			return parameterName(1);
 		}	
 	}
@@ -17,4 +17,4 @@ var username = getURLParameters('username';
 		username = 'Anonymous_'+Math.random();
 	}
 
-$('#messages').append('<h4>'+username+('username')+'</h4>');
+$('#messages').append('<h4>'+getURLParameters('username')+'</h4>');
